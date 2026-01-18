a174fccc74cc1f65ff25dafdeaecf239fdcb2a4c
Icemaster67 <angiejoyaa16@gmail.com>
2026-01-18 17:53:55 -0500

    Create elif.py

---
+++ elif.py
@@ -0,0 +1,26 @@
+country = input("What country are you from?: ")
+
+if country =="India":
+    print("That is the the first ranked country in the world also it has the Highest population")
+elif country == "China":
+    print("That is the Second highest population, high economic power" )
+elif country == "USA":
+    print("That country has Major global economic/military power")
+elif country == "Indonesia":
+    print("That country is the Fourth most populous")
+elif country == "Pakistan":
+    print("That country is the Fifth most populous")
+elif country == "Nigeria":
+    print ("That country is Rapidly growing African population")
+elif country == "Brazil":
+    print("That country is the Largest country in South America")
+elif country == "Bangladesh":
+    print("That country has High population density")
+elif country == "Russia":
+    print("That country is the Largest country by area")
+elif country == "Mexico":
+    print("That country has High population and economic activity")
+elif country =="Japan":
+    print("That country has High technology and top tourist destination")
+elif country == "Canada":
+    print("That country is Highly ranked for quality of life/tourism")
